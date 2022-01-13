da2a59e3710354995a5f3652e746bf7b4a2b0625
Fedor Logvinov <fedor.logvinov@unqork.com>
2022-01-13 16:51:20 -0500

    trying to trigger sonar qube

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {sonarTest} from "./sonar_qube_test";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {sonarTest('i am A', "i am B")}
         </p>
         <a
           className="App-link"
